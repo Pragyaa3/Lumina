c3e1d7ce708b7c1306dfa201df274c6c96ba769e
Pragya Hurmade <pragyahurmade2226@gmail.com>
2025-09-15 22:58:32 +0530

    Tailwind integration

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,8 +1,12 @@
 export default function Home() {
-  return (
-    <div style={{ fontFamily: "sans-serif", textAlign: "center", marginTop: "50px" }}>
-      <h1>Welcome to Lumina ✨</h1>
-      <p>Your decentralized project starts here!</p>
-    </div>
-  );
+    return (
+        <div style={{ fontFamily: "sans-serif", textAlign: "center", marginTop: "50px" }}>
+            <h1>Welcome to Lumina ✨</h1>
+            <p>Your decentralized project starts here!</p>
+
+            <h1 className="text-5xl font-bold">🚀 Tailwind is Working!</h1>
+
+        </div>
+
+    );
 }
